23c9238b9e5f658431ff6105e9ed5013e395dc08
tanrbobanr <87777314+tanrbobanr@users.noreply.github.com>
2022-10-09 13:21:31 -0700

    added ability for OverloadDict to be cleaned/transformed

--- src/httpprep/OverloadDict.py
+++ src/httpprep/OverloadDict.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import typing
 
 
@@ -16,6 +17,8 @@ class OverloadDict(dict[typing.Any, OverloadList]):
     d = OverloadDict()
     d["a", "b", "c"] = 1, 2, 3
     ```
+    Unwanted values may be removed through the `remove_values` method, and can be
+    converted into a standard `dict` object through the `to_dict` method.
 
     """
     def __init__(self, *args, **kwargs) -> None:
@@ -30,3 +33,30 @@ class OverloadDict(dict[typing.Any, OverloadList]):
                 self[k].append(v) if __k in self else super().__setitem__(k, OverloadList([v]))
             return
         return self[__k].append(__v) if __k in self else super().__setitem__(__k, OverloadList([__v]))
+
+
+    def remove_values(self, remove: typing.Any) -> OverloadDict:
+        """Iteratively removes values in each of the contained `OverloadList` objects
+        that are equal to `remove` and returns the result as a new `OverloadDict`.
+        
+        """
+        new = {}
+        for k, v_list in self.items():
+            temp = [v for v in v_list if v != remove]
+            if temp:
+                new[k] = OverloadList(temp)
+        return OverloadDict(new)
+    
+
+    def to_dict(self, pref: int = 0) -> dict:
+        """Iteratively creates a new `dict` object with the contents if this OverloadDict.
+        A list index preference may be chosen through the `pref` parameter, and must be
+        either `0` (first item) or `-1` (last item).
+        
+        """
+        new = {}
+        for k, v_list in self.items():
+            temp = v_list[pref] if len(v_list) > 0 else None
+            if temp:
+                new[k] = temp
+        return new
